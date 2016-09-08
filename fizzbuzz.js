31c7a8e226b668de998c0fb4eb6fd96975b4c512
Nathan Bland <nathan.bland@gmail.com>
2016-09-08 08:51:44 -0600

    all the cool kids use es6

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,9 @@
-function fizzbuzz(max){
-  for (var i=1; i<max+1; i++){
-    var str = ''
-    if (i % 3 === 0){
-      str+= 'Fizz'
-    }
-    if (i % 5 === 0){
-      str+= 'Buzz'
-    }
-    if (str === ''){
-      str = i
-    }
+const fizzbuzz = (max) => {
+  for (let i = 1; i < max + 1; i++) {
+    let str = ''
+    i % 3 === 0 ? str += 'Fizz' : ''
+    i % 5 === 0 ? str += 'Buzz' : ''
+    str === '' ? str = i : ''
     console.log(str)
   }
 }
